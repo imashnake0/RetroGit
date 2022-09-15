ebf06cd00b5aa51a9374f59d7c67b0f9becf4ad8
imashnake0 <kamaleshpaluru07@gmail.com>
2022-09-15 01:49:31 -0400

    Created `Repo`
    This is a model for each of the items in the list at https://api.github.com/users/{user}/repos.

---
+++ app/src/main/java/com/imashnake0/retrogit/data/models/Repo.kt
@@ -0,0 +1,10 @@
+package com.imashnake0.retrogit.data.models
+
+data class Repo(
+    val id: Int,
+    val name: String,
+    val url: String,
+    val description: String,
+    val topics: List<String>,
+    val stars: Int
+)
