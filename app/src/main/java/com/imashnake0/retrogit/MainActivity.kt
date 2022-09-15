6aeb6cf3ab0c66b55a5d65ed571bb694dd37c8ce
imashnake0 <kamaleshpaluru07@gmail.com>
2022-09-15 02:27:32 -0400

    Logged the response

--- app/src/main/java/com/imashnake0/retrogit/MainActivity.kt
+++ app/src/main/java/com/imashnake0/retrogit/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.imashnake0.retrogit
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,11 +9,32 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.lifecycleScope
+import com.imashnake0.retrogit.data.apis.GitHubApi
 import com.imashnake0.retrogit.features.theme.RetroGitTheme
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val retrofit = Retrofit
+            .Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl("https://api.github.com/")
+            .build()
+
+        val api = retrofit.create(GitHubApi::class.java)
+
+        lifecycleScope.launch(Dispatchers.IO) {
+            api.fetchRepos("imashnake0").forEach {
+                Log.d("ResponseBlah", it.toString())
+            }
+        }
+
         setContent {
             RetroGitTheme {
                 // A surface container using the 'background' color from the theme
